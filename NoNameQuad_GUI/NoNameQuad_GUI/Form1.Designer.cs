ae29502117e8f50ad6dab68e5709bac072c09559
Zerb <hossamvs911@gmail.com>
2017-10-22 00:06:20 +0200

    added tab 3

--- NoNameQuad_GUI/NoNameQuad_GUI/Form1.Designer.cs
+++ NoNameQuad_GUI/NoNameQuad_GUI/Form1.Designer.cs
@@ -34,7 +34,14 @@
             this.tabPage3 = new System.Windows.Forms.TabPage();
             this.tabPage4 = new System.Windows.Forms.TabPage();
             this.tabPage5 = new System.Windows.Forms.TabPage();
+            this.label_Serv90_t3 = new System.Windows.Forms.Label();
+            this.label_Serv180_t3 = new System.Windows.Forms.Label();
+            this.label_Serv0_t3 = new System.Windows.Forms.Label();
+            this.button_Serv90_t3 = new System.Windows.Forms.Button();
+            this.button_Serv180_t3 = new System.Windows.Forms.Button();
+            this.button_Serv0_t3 = new System.Windows.Forms.Button();
             this.tabControl1.SuspendLayout();
+            this.tabPage3.SuspendLayout();
             this.SuspendLayout();
             // 
             // tabControl1
@@ -65,13 +72,19 @@
             this.tabPage2.Location = new System.Drawing.Point(4, 22);
             this.tabPage2.Name = "tabPage2";
             this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage2.Size = new System.Drawing.Size(192, 74);
+            this.tabPage2.Size = new System.Drawing.Size(597, 272);
             this.tabPage2.TabIndex = 1;
             this.tabPage2.Text = "tabPage2";
             this.tabPage2.UseVisualStyleBackColor = true;
             // 
             // tabPage3
             // 
+            this.tabPage3.Controls.Add(this.button_Serv0_t3);
+            this.tabPage3.Controls.Add(this.button_Serv180_t3);
+            this.tabPage3.Controls.Add(this.button_Serv90_t3);
+            this.tabPage3.Controls.Add(this.label_Serv0_t3);
+            this.tabPage3.Controls.Add(this.label_Serv180_t3);
+            this.tabPage3.Controls.Add(this.label_Serv90_t3);
             this.tabPage3.Location = new System.Drawing.Point(4, 22);
             this.tabPage3.Name = "tabPage3";
             this.tabPage3.Padding = new System.Windows.Forms.Padding(3);
@@ -100,6 +113,62 @@
             this.tabPage5.Text = "tabPage5";
             this.tabPage5.UseVisualStyleBackColor = true;
             // 
+            // label_Serv90_t3
+            // 
+            this.label_Serv90_t3.AutoSize = true;
+            this.label_Serv90_t3.Location = new System.Drawing.Point(11, 30);
+            this.label_Serv90_t3.Name = "label_Serv90_t3";
+            this.label_Serv90_t3.Size = new System.Drawing.Size(115, 13);
+            this.label_Serv90_t3.TabIndex = 0;
+            this.label_Serv90_t3.Text = "Calibrate Servos to 90°";
+            // 
+            // label_Serv180_t3
+            // 
+            this.label_Serv180_t3.AutoSize = true;
+            this.label_Serv180_t3.Location = new System.Drawing.Point(11, 70);
+            this.label_Serv180_t3.Name = "label_Serv180_t3";
+            this.label_Serv180_t3.Size = new System.Drawing.Size(121, 13);
+            this.label_Serv180_t3.TabIndex = 1;
+            this.label_Serv180_t3.Text = "Calibrate Servos to 180°";
+            this.label_Serv180_t3.Click += new System.EventHandler(this.Serv180_Click);
+            // 
+            // label_Serv0_t3
+            // 
+            this.label_Serv0_t3.AutoSize = true;
+            this.label_Serv0_t3.Location = new System.Drawing.Point(11, 110);
+            this.label_Serv0_t3.Name = "label_Serv0_t3";
+            this.label_Serv0_t3.Size = new System.Drawing.Size(109, 13);
+            this.label_Serv0_t3.TabIndex = 2;
+            this.label_Serv0_t3.Text = "Calibrate Servos to 0°";
+            // 
+            // button_Serv90_t3
+            // 
+            this.button_Serv90_t3.Location = new System.Drawing.Point(148, 30);
+            this.button_Serv90_t3.Name = "button_Serv90_t3";
+            this.button_Serv90_t3.Size = new System.Drawing.Size(75, 23);
+            this.button_Serv90_t3.TabIndex = 3;
+            this.button_Serv90_t3.Text = "Wosh";
+            this.button_Serv90_t3.UseVisualStyleBackColor = true;
+            // 
+            // button_Serv180_t3
+            // 
+            this.button_Serv180_t3.Location = new System.Drawing.Point(148, 70);
+            this.button_Serv180_t3.Name = "button_Serv180_t3";
+            this.button_Serv180_t3.Size = new System.Drawing.Size(75, 23);
+            this.button_Serv180_t3.TabIndex = 4;
+            this.button_Serv180_t3.Text = "Shazam";
+            this.button_Serv180_t3.UseVisualStyleBackColor = true;
+            // 
+            // button_Serv0_t3
+            // 
+            this.button_Serv0_t3.Location = new System.Drawing.Point(148, 110);
+            this.button_Serv0_t3.Name = "button_Serv0_t3";
+            this.button_Serv0_t3.Size = new System.Drawing.Size(75, 23);
+            this.button_Serv0_t3.TabIndex = 5;
+            this.button_Serv0_t3.Text = "idk";
+            this.button_Serv0_t3.UseVisualStyleBackColor = true;
+            this.button_Serv0_t3.Click += new System.EventHandler(this.button3_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -109,6 +178,8 @@
             this.Name = "Form1";
             this.Text = "DEVELOPMENT GUI ";
             this.tabControl1.ResumeLayout(false);
+            this.tabPage3.ResumeLayout(false);
+            this.tabPage3.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -121,6 +192,12 @@
         private System.Windows.Forms.TabPage tabPage3;
         private System.Windows.Forms.TabPage tabPage4;
         private System.Windows.Forms.TabPage tabPage5;
+        private System.Windows.Forms.Label label_Serv90_t3;
+        private System.Windows.Forms.Button button_Serv0_t3;
+        private System.Windows.Forms.Button button_Serv180_t3;
+        private System.Windows.Forms.Button button_Serv90_t3;
+        private System.Windows.Forms.Label label_Serv0_t3;
+        private System.Windows.Forms.Label label_Serv180_t3;
     }
 }
 
--- NoNameQuad_GUI/NoNameQuad_GUI/Form1.cs
+++ NoNameQuad_GUI/NoNameQuad_GUI/Form1.cs
@@ -16,5 +16,15 @@ namespace NoNameQuad_GUI
         {
             InitializeComponent();
         }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Serv180_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
