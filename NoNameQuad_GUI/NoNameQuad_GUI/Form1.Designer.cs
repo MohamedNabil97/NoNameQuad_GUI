7e3188d762458b08b929e1dfd17b141eb647c352
MohamedYasser97 <mhmd.yasser@gmail.com>
2017-10-22 00:17:33 +0200

    added design of tab 2

--- NoNameQuad_GUI/NoNameQuad_GUI/Form1.Designer.cs
+++ NoNameQuad_GUI/NoNameQuad_GUI/Form1.Designer.cs
@@ -34,7 +34,23 @@
             this.tabPage3 = new System.Windows.Forms.TabPage();
             this.tabPage4 = new System.Windows.Forms.TabPage();
             this.tabPage5 = new System.Windows.Forms.TabPage();
+            this.label_legNumber_t2 = new System.Windows.Forms.Label();
+            this.label_currCoordinates_t2 = new System.Windows.Forms.Label();
+            this.label_xCoord_t2 = new System.Windows.Forms.Label();
+            this.label_yCoord_t2 = new System.Windows.Forms.Label();
+            this.label_zCoord_t2 = new System.Windows.Forms.Label();
+            this.label_xHold_t2 = new System.Windows.Forms.Label();
+            this.label_yHold_t2 = new System.Windows.Forms.Label();
+            this.label_zHold_t2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button_apply_t2 = new System.Windows.Forms.Button();
+            this.comboBox_legChoice_t2 = new System.Windows.Forms.ComboBox();
+            this.pictureBox_servos_t2 = new System.Windows.Forms.PictureBox();
             this.tabControl1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_servos_t2)).BeginInit();
             this.SuspendLayout();
             // 
             // tabControl1
@@ -62,10 +78,24 @@
             // 
             // tabPage2
             // 
+            this.tabPage2.Controls.Add(this.pictureBox_servos_t2);
+            this.tabPage2.Controls.Add(this.comboBox_legChoice_t2);
+            this.tabPage2.Controls.Add(this.button_apply_t2);
+            this.tabPage2.Controls.Add(this.textBox3);
+            this.tabPage2.Controls.Add(this.textBox2);
+            this.tabPage2.Controls.Add(this.textBox1);
+            this.tabPage2.Controls.Add(this.label_zHold_t2);
+            this.tabPage2.Controls.Add(this.label_yHold_t2);
+            this.tabPage2.Controls.Add(this.label_xHold_t2);
+            this.tabPage2.Controls.Add(this.label_zCoord_t2);
+            this.tabPage2.Controls.Add(this.label_yCoord_t2);
+            this.tabPage2.Controls.Add(this.label_xCoord_t2);
+            this.tabPage2.Controls.Add(this.label_currCoordinates_t2);
+            this.tabPage2.Controls.Add(this.label_legNumber_t2);
             this.tabPage2.Location = new System.Drawing.Point(4, 22);
             this.tabPage2.Name = "tabPage2";
             this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage2.Size = new System.Drawing.Size(192, 74);
+            this.tabPage2.Size = new System.Drawing.Size(597, 272);
             this.tabPage2.TabIndex = 1;
             this.tabPage2.Text = "tabPage2";
             this.tabPage2.UseVisualStyleBackColor = true;
@@ -100,6 +130,126 @@
             this.tabPage5.Text = "tabPage5";
             this.tabPage5.UseVisualStyleBackColor = true;
             // 
+            // label_legNumber_t2
+            // 
+            this.label_legNumber_t2.AutoSize = true;
+            this.label_legNumber_t2.Location = new System.Drawing.Point(39, 26);
+            this.label_legNumber_t2.Name = "label_legNumber_t2";
+            this.label_legNumber_t2.Size = new System.Drawing.Size(65, 13);
+            this.label_legNumber_t2.TabIndex = 0;
+            this.label_legNumber_t2.Text = "Leg Number";
+            // 
+            // label_currCoordinates_t2
+            // 
+            this.label_currCoordinates_t2.AutoSize = true;
+            this.label_currCoordinates_t2.Location = new System.Drawing.Point(39, 63);
+            this.label_currCoordinates_t2.Name = "label_currCoordinates_t2";
+            this.label_currCoordinates_t2.Size = new System.Drawing.Size(100, 13);
+            this.label_currCoordinates_t2.TabIndex = 1;
+            this.label_currCoordinates_t2.Text = "Current Coordinates";
+            // 
+            // label_xCoord_t2
+            // 
+            this.label_xCoord_t2.AutoSize = true;
+            this.label_xCoord_t2.Location = new System.Drawing.Point(140, 88);
+            this.label_xCoord_t2.Name = "label_xCoord_t2";
+            this.label_xCoord_t2.Size = new System.Drawing.Size(48, 13);
+            this.label_xCoord_t2.TabIndex = 2;
+            this.label_xCoord_t2.Text = "X: NULL";
+            // 
+            // label_yCoord_t2
+            // 
+            this.label_yCoord_t2.AutoSize = true;
+            this.label_yCoord_t2.Location = new System.Drawing.Point(216, 88);
+            this.label_yCoord_t2.Name = "label_yCoord_t2";
+            this.label_yCoord_t2.Size = new System.Drawing.Size(48, 13);
+            this.label_yCoord_t2.TabIndex = 3;
+            this.label_yCoord_t2.Text = "Y: NULL";
+            // 
+            // label_zCoord_t2
+            // 
+            this.label_zCoord_t2.AutoSize = true;
+            this.label_zCoord_t2.Location = new System.Drawing.Point(287, 88);
+            this.label_zCoord_t2.Name = "label_zCoord_t2";
+            this.label_zCoord_t2.Size = new System.Drawing.Size(48, 13);
+            this.label_zCoord_t2.TabIndex = 4;
+            this.label_zCoord_t2.Text = "Z: NULL";
+            // 
+            // label_xHold_t2
+            // 
+            this.label_xHold_t2.AutoSize = true;
+            this.label_xHold_t2.Location = new System.Drawing.Point(61, 119);
+            this.label_xHold_t2.Name = "label_xHold_t2";
+            this.label_xHold_t2.Size = new System.Drawing.Size(14, 13);
+            this.label_xHold_t2.TabIndex = 5;
+            this.label_xHold_t2.Text = "X";
+            // 
+            // label_yHold_t2
+            // 
+            this.label_yHold_t2.AutoSize = true;
+            this.label_yHold_t2.Location = new System.Drawing.Point(61, 161);
+            this.label_yHold_t2.Name = "label_yHold_t2";
+            this.label_yHold_t2.Size = new System.Drawing.Size(14, 13);
+            this.label_yHold_t2.TabIndex = 6;
+            this.label_yHold_t2.Text = "Y";
+            // 
+            // label_zHold_t2
+            // 
+            this.label_zHold_t2.AutoSize = true;
+            this.label_zHold_t2.Location = new System.Drawing.Point(61, 207);
+            this.label_zHold_t2.Name = "label_zHold_t2";
+            this.label_zHold_t2.Size = new System.Drawing.Size(14, 13);
+            this.label_zHold_t2.TabIndex = 7;
+            this.label_zHold_t2.Text = "Z";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(143, 119);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 8;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(143, 158);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 9;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(143, 200);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 10;
+            // 
+            // button_apply_t2
+            // 
+            this.button_apply_t2.Location = new System.Drawing.Point(234, 231);
+            this.button_apply_t2.Name = "button_apply_t2";
+            this.button_apply_t2.Size = new System.Drawing.Size(92, 23);
+            this.button_apply_t2.TabIndex = 11;
+            this.button_apply_t2.Text = "Apply Changes";
+            this.button_apply_t2.UseVisualStyleBackColor = true;
+            // 
+            // comboBox_legChoice_t2
+            // 
+            this.comboBox_legChoice_t2.FormattingEnabled = true;
+            this.comboBox_legChoice_t2.Location = new System.Drawing.Point(143, 18);
+            this.comboBox_legChoice_t2.Name = "comboBox_legChoice_t2";
+            this.comboBox_legChoice_t2.Size = new System.Drawing.Size(100, 21);
+            this.comboBox_legChoice_t2.TabIndex = 12;
+            // 
+            // pictureBox_servos_t2
+            // 
+            this.pictureBox_servos_t2.Image = global::NoNameQuad_GUI.Properties.Resources.leg_planes;
+            this.pictureBox_servos_t2.Location = new System.Drawing.Point(353, 43);
+            this.pictureBox_servos_t2.Name = "pictureBox_servos_t2";
+            this.pictureBox_servos_t2.Size = new System.Drawing.Size(224, 191);
+            this.pictureBox_servos_t2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox_servos_t2.TabIndex = 13;
+            this.pictureBox_servos_t2.TabStop = false;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -109,6 +259,9 @@
             this.Name = "Form1";
             this.Text = "DEVELOPMENT GUI ";
             this.tabControl1.ResumeLayout(false);
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_servos_t2)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -121,6 +274,20 @@
         private System.Windows.Forms.TabPage tabPage3;
         private System.Windows.Forms.TabPage tabPage4;
         private System.Windows.Forms.TabPage tabPage5;
+        private System.Windows.Forms.Label label_legNumber_t2;
+        private System.Windows.Forms.Button button_apply_t2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label_zHold_t2;
+        private System.Windows.Forms.Label label_yHold_t2;
+        private System.Windows.Forms.Label label_xHold_t2;
+        private System.Windows.Forms.Label label_zCoord_t2;
+        private System.Windows.Forms.Label label_yCoord_t2;
+        private System.Windows.Forms.Label label_xCoord_t2;
+        private System.Windows.Forms.Label label_currCoordinates_t2;
+        private System.Windows.Forms.PictureBox pictureBox_servos_t2;
+        private System.Windows.Forms.ComboBox comboBox_legChoice_t2;
     }
 }
 
